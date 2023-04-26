acc8146853029649168ca7baef8c6a9640d07f0c
handaehee93 <hdh8990@naver.com>
2023-04-26 18:03:51 +0900

    클라우디너리에 이미지 등록

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Button from '../components/ui/Button'
+import { uploadImage } from '../api/uploader'
 export default function NewProduct() {
+  const [product, setProduct] = useState({})
+  const [file, setFile] = useState()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    uploadImage(file)
+      .then(url => {console.log(url)})
+  }
+
+  const handleChange = (e) => {
+    const {name, value, files} = e.target
+    if(name === 'file') {
+      setFile(files && files[0])
+      return
+    }
+    setProduct((product) => ({...product, [name]: value}))
+  }
+
   return (
-    <div>NewProduct</div>
+    <div>
+      {file && <img src={URL.createObjectURL(file)} alt='local' />}
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept='image/*' name='file' onChange={handleChange} required />
+        <input type="text" name='title' value={product.title ?? ''} placeholder='제품명' required onChange={handleChange} />
+        <input type="number" name='price' value={product.price ?? ''} placeholder='가격' required onChange={handleChange}/>
+        <input type="text" name='category' value={product.category ?? ''} placeholder='카테고리' required onChange={handleChange}/>
+        <input type="text" name='description' value={product.description ?? ''} placeholder='제품 설명' required onChange={handleChange}/>
+        <input type="text" name='options' value={product.options ?? ''} placeholder='옵션' required onChange={handleChange}/>
+        <Button text='제품 등록 하기'/>
+      </form>
+    </div>
   )
 }
+
