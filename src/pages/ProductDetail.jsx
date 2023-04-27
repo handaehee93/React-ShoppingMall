fdac4e9ac44f31038f6f8dfeda49ef1bfb67f285
handaehee93 <hdh8990@naver.com>
2023-04-27 14:21:52 +0900

    상품 디페일 페이지 Ui 구현

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
-
+import { useLocation } from 'react-router-dom'
+import Button from '../components/ui/Button'
 export default function ProductDetail() {
+  // useLoacation을 활용하여 useParam으로 전달된 state를 받아 옴
+  const {state: {
+    product: {id, image, title, category,price, description, options }
+  }} = useLocation()
+
   return (
-    <div>ProductDetail</div>
+    <div>
+      <p className='mx-12 mt-4 text-gray-700'>{category}</p>
+      <section className='flex flex-col md:flex-row p-4'>
+        <img className='w-full basis-7/12' src={image} alt={title} />
+        <div className='basis-5/12 flex flex-col p-4'>
+          <h2 className='text-3xl font-bold py-2 border-b border-gray-300'>{title}</h2>
+          <p className='text-2xl font-bold py-2'>$ {price} </p>
+          <p>{description}</p>
+          <Button text='장바구니에 추가'/>
+        </div>
+      </section>
+    </div>
   )
 }
+
