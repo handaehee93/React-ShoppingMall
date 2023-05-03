a67fa168a97d383fce09557b3119eb50df24af3e
handaehee93 <hdh8990@naver.com>
2023-05-03 18:50:24 +0900

    cart ui 완성

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { AddUpdateToCart } from '../api/firebase'
+import { removeFromCart } from '../api/firebase'
+import Button from './ui/Button'
+
+export default function CartItem({product, user}) {
+  const {id, image, title, quantity, price} = product
+
+  const handlePlus = () => {
+    AddUpdateToCart(user.uid, {...product, quantity: quantity +1})
+
+  }
+  const handleMinus = () => {
+    if(quantity < 2) {
+      return
+    }
+    AddUpdateToCart(user.uid, {...product, quantity: quantity -1})
+  }
+  const handleDelete = () => {
+    removeFromCart(user.uid, id)
+  }
+  return (
+    <li className='flex justify-between my-2 items-center'>
+      <img className='w-24 md:w-48 rounded-lg' src={image} alt="이미지" />
+      <div className='flex-1 ml-4 justify-between'>
+        <div className='basis-3/5'>
+          <p className='text-lg'>{title}</p>
+          <p className=''>${price}</p>
+        </div>
+        <div className='text-2xl flex  gap-2 items-center'>
+          <Button 
+            className='cursor-pointer transition-all hover:text-logo mx-1' text='-' 
+            onClick={handleMinus} 
+          />
+          <span>{quantity}</span>
+          <Button 
+            className='cursor-pointer transition-all hover:text-logo mx-1' 
+            text='+' 
+            onClick={handlePlus} 
+          />
+          <Button 
+            className='cursor-pointer transition-all hover:text-logo mx-1' 
+            text='삭제' 
+            onClick={handleDelete} 
+          />
+        </div>
+      </div>
+    </li>
+  )
+}
+
+
